a979ca0e147e5bcbd15a8f01e23b2e22dd359b1c
marioAratech <mario.azcunaga@ara-tech.es>
2024-04-17 12:03:54 +0200

    inicio

---
+++ app/Console/Commands/ComandoPrueba.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ComandoPrueba extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:comando-prueba';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
